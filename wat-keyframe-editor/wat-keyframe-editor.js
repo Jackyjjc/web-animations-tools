984b75a74177de1a6b75b99451b43dd7d6a12182
jackyjjc <jackyjjc@google.com>
2013-12-12 00:07:34 +1100

    checkpoint

--- wat-keyframe-editor/wat-keyframe-editor.js
+++ wat-keyframe-editor/wat-keyframe-editor.js
@@ -1,73 +1,72 @@
 /*
-Copyright 2013 Google Inc. All Rights Reserved.
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-
-limitations under the License.
-*/
+'use strict;'
 
 Polymer('wat-keyframe-editor', {
-
-  //the keyframe effect object
+  // This is the keyframe effect object.
   effect: null,
   
-  //the frames and properties that are currently being edited
+  // These are the frames and properties currently being edited.
   frames: null,
   properties: null,
 
-  //record all known properties to prevent user adding redundant properties
-  //can be done by searching through the properties array but this is free
-  //as we already generated it on initialization
-  found: null,
+  // It record all seen properties in the keyframes to prevent redundant properties.
+  // We can achieve the same effect by searching through the properties array
+  // but we already generate this map in initialization so we can just use it.
+  seenProperties: null,
 
-  //when effect is changed, update the fields to reflect the changes
+  // When effect is changed, update the fields to reflect the changes.
   effectChanged: function(oldValue, newValue) {
 
     var frames = this.effect.getFrames();
 
-    //need to loop through all the frames to get all the properties
-    //because each frame only have a subset of them
-    var found = {};
+    // We need to loop through the frames to get all the properties
+    // because each frame may only have a subset.
+    var seenProperties = {};
     var properties = [];
 
-    var i, prop;
-    for (i = 0; i < frames.length; i++) {
+    var prop;
+    for (var i = 0; i < frames.length; i++) {
       for (prop in frames[i]) {
             
-        if (found[prop]) {
+        if (seenProperties[prop]) {
           continue;
         }
 
-        found[prop] = true;
+        seenProperties[prop] = true;
         properties.push(prop);
       }
     }
 
     this.frames = frames;
     this.properties = properties;
-    this.found = found;
+    this.seenProperties = seenProperties;
   },
   
   cellValueChanged: function(e) {
-
-    //find the corresponding frame and property of the changing cell
-    var frameIndex = e.target.dataset["column"];
-    var propIndex = e.target.dataset["row"];
+    // Find the corresponding frame and property of the changing cell.
+    var frameIndex = e.target.dataset['column'];
+    var propIndex = e.target.dataset['row'];
 
     var prop = this.properties[propIndex];
     var frame = this.frames[frameIndex];
 
     var value = e.target.value;
-    if (prop === "offset") {
+    if (prop === 'offset') {
       value = parseFloat(value);
     }
     frame[prop] = value;
@@ -76,43 +75,39 @@ Polymer('wat-keyframe-editor', {
   },
 
   gainFocus: function(e) {
-
-    //the input element get focus but the parent div get highlighted
+    // The input element gets the focus but the parent get highlighted.
     var cell = e.target.parentNode;
 
-    document.timeline.play(
-            new Animation(cell, 
-                          [{transform:"scale(1)"},
-                           {transform:"scale(1.1)"}, 
-                           {transform:"scale(1)"}], 
-                          0.1)
-    );
+    cell.animate([
+      {transform:'scale(1)'},
+      {transform:'scale(1.1)'}, 
+      {transform:'scale(1)'}
+    ], 0.1);
 
-    cell.style.border = "1px solid blue";
+    cell.style.border = '1px solid blue';
   },
 
   loseFocus: function(e) {
-
-    //the input element get focus but the parent div get highlighted
+    // The input element gets the focus but the parent get highlighted.
     var cell = e.target.parentNode;
 
-    cell.style.border = "1px solid black";
+    cell.style.border = '1px solid black';
   },
 
   actionOnFrame: function(e) {
-        
     var action = this.getAction(e);
     if (!action) {
       return;
     }
 
-    //get the index of the header on which the action is performing
-    var index = parseInt(e.target.dataset["index"]);
+    // Get the index of the header on which the action being performed.
+    var index = parseInt(e.target.dataset['index']);
     
-    if (action === "insert") {
-      //insert an empty frame after the current frame
+    if (action === 'insert') {
+      // We decide to insert an empty frame AFTER 
+      // the current frame and that is where the +1 come from.
       this.frames.splice(index + 1, 0, {});
-    } else if (action === "delete") {
+    } else if (action === 'delete') {
       this.frames.splice(index, 1);
     }
 
@@ -120,38 +115,39 @@ Polymer('wat-keyframe-editor', {
   },
 
   actionOnProperty: function(e) {
+    
+    var ENTER_KEY_CODE = 13;
+    var action;
 
-    var action = null;
-    //13 is the enter key. When enter is pressed, insert the property
-    if (e.type === "keyup" && e.keyCode === 13) {
-      action = "insert";
+    if (e.type === 'keyup' && e.keyCode === ENTER_KEY_CODE) {
+      action = 'insert';
     } else {
       action = this.getAction(e);
     }
 
-    if (!action) {
+    if (action !== 'insert' || action !== 'delete') {
       return;
     }
   
-    if (action === "insert") {
+    if (action === 'insert') {
       var prop = e.target.value;
-      if (this.found[prop]) {
+      if (this.seenProperties[prop]) {
         return;
       }
 
       this.properties.push(prop);
 
-      //reset the input element back to empty
-      e.target.value = "";
+      // Reset the input element back to empty.
+      e.target.value = '';
 
-    } else if (action === "delete") {
+    } else if (action === 'delete') {
       
-      var index = parseInt(e.target.dataset["index"]);
+      var index = parseInt(e.target.dataset['index']);
       var prop = this.properties[index];
       
       this.properties.splice(index, 1);
-      //remove the property from all the frames
-      for(var i = 0; i < this.frames.length; i++) {
+      // Remove the property from all the frames.
+      for (var i = 0; i < this.frames.length; i++) {
         delete this.frames[i][prop];
       }
     }
@@ -161,19 +157,21 @@ Polymer('wat-keyframe-editor', {
 
   getAction: function(e) {
 
-    var action = null;
+    var INSERT_KEY_CODE = 45;
+    var DELETE_KEY_CODE = 46;
 
-    if (e.type === "click") {
-      action = e.target.dataset["action"];
+    if (e.type === 'click') {
+      return e.target.dataset['action'];
+    }
 
-    } else if (e.type === "keyup") {
+    if (e.type === 'keyup') {
       switch(e.keyCode) {
-        case 45: action = "insert"; break; //45 insert key
-        case 46: action = "delete"; break; //46 delete key
-        default: break;
+        case INSERT_KEY_CODE: 
+          return 'insert';
+        case DELETE_KEY_CODE: 
+          return 'delete';
+        default:
+          return null;
       }
     } 
-
-    return action; 
-  }
 });
